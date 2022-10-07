29d3521b2fd5e4b66d061081dc06ad99334e5dd1
Jonian06 <100470090+Jonian06@users.noreply.github.com>
2022-10-07 16:45:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,106 @@
+canvas = document.getElementById('myCanvas');
+ctx=canvas.getcontext("2d");
+
+greencar_width=75;
+greencar_height=100;
+
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+greencar_x=5;
+greencar_y=225;
+
+function add() {
+background_imgTag= new Image();
+background_img.onload= uploadBackground;
+background_img.src= background_image;
+
+greencar_imgTag= new Image();
+greencar_img.onload= uploadgreencar;
+greencar_img.src= greencar_image;
+}
+
+function uploadBackground() {
+ctx.drawImage(background_imgTag, 0 , 0, canvas.width , canvas.heigt)
+
+}
+
+function uploadgreencar() {
+	ctx.drawImage(greencar_imgTag, greencar_x , greencar_y, greencar.width , greencar.heigt)
+
+	
+}
+
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+	if (greencar_y <=0){
+		greencar_y = greencar_x = 10;
+		cconsole.log("when up arrow is pressed, X ="+greencar_x+", Y ="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+
+function down()
+{
+	if (greencar_y >=0){
+		greencar_y = greencar_x = 10;
+		cconsole.log("when down arrow is pressed, X ="+greencar_x+", Y ="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+
+function left()
+{
+	if (greencar_y <=0){
+		greencar_y = greencar_x = 10;
+		cconsole.log("when left arrow is pressed, X ="+greencar_x+", Y ="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
+
+function right()
+{
+	if (greencar_y >=0){
+		greencar_y = greencar_x = 10;
+		cconsole.log("when right arrow is pressed, X ="+greencar_x+", Y ="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
+}
